81bb2a2457c640e3ffcc21c7645b9d81f5cd30c0
Rachit Anklesaria <rachita190899@gmail.com>
2024-10-17 14:27:45 +0100

    Data is randomly generated 6 letter strings. AngularApp now uses localStorage to store and read data.

--- AngularApp/src/app/app.component.ts
+++ AngularApp/src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 declare function buildData(num: any): any;
-declare function deleteData(): any;
 
 @Component({
   selector: 'app-root',
@@ -13,12 +12,27 @@ declare function deleteData(): any;
 })
 export class AppComponent {
   title = 'AngularApp';
+
   data = [];
+  display = false;
+
   generateData(num: any) {
     this.data = buildData(num);
+    localStorage.setItem("data", JSON.stringify(this.data));
+    this.data = [];
   }
 
   clearData() {
-    this.data = deleteData();
+    this.data = [];
+    localStorage.removeItem("data");
+    this.display = false;
+  }
+
+  displayData() {
+    this.display = true;
+    var str_data = localStorage.getItem("data");
+    if (str_data !== null) {
+      this.data = JSON.parse(str_data);
+    }
   }
 }
--- AngularApp/src/app/dummyData.js
+++ AngularApp/src/app/dummyData.js
@@ -2,23 +2,17 @@ function ranNum(max) {
     return Math.floor(Math.random() * (max));
 }
 
-dataSet1 = ["attractive", "clean", "dazzling", "elegant", "fancy", "gorgeous",
-    "handsome", "long", "short", "alive", "helpful", "important", "odd", "poor", "rich", "tender", "vast", "wrong", "aggressive", "brave", "calm", "delightful", "happy", "proud", "silly", "thankful", "witty", "angry", "clumsy", "defeated", "mysterious", "big", "gigantic", "huge", "immense", "large", "rhythmic", "sweet"];
-
-dataSet2 = ["black", "blue", "brown", "gray", "green", "orange", "red", "violet", "white", "yellow", "magenta", "purple", "teal"];
-
-dataSet3 = ["bench", "chair", "mountain", "castle", "computer", "bottle", "tower", "sunglasses", "deodorant", "car", "palace", "brush", "wallet", "charger", "scissor", "notebook", "wire", "belt", "popcorn", "umbrella", "basket"];
+chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+len = 6;
 
 function buildData(size) {
     data = new Array(size);
     for (let i = 0; i < size; i++) {
-        data[i] = dataSet1[ranNum(dataSet1.length)] + " " + dataSet2[ranNum(dataSet2.length)] + " " + dataSet3[ranNum(dataSet3.length)];
+        let str = "";
+        for (let j = 0; j < len; j++) {
+            str += chars.charAt(ranNum(chars.length));
+        }
+        data[i] = str;
     }
-    console.log(size);
-    return data;
-}
-
-function deleteData() {
-    data = [];
     return data;
 }

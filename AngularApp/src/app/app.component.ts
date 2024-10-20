18134d383c4cf2aec0cfdd4e7936325bb33e99a8
Rachit Anklesaria <rachita190899@gmail.com>
2024-10-20 12:48:50 +0100

    Removed dependency on JavaScript file for creating records. Added more test cases.

--- AngularApp/src/app/app.component.ts
+++ AngularApp/src/app/app.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-declare function buildData(num: any): any;
-
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -13,13 +11,44 @@ declare function buildData(num: any): any;
 export class AppComponent {
   title = 'AngularApp';
 
-  data = [];
+  data: string[] = [];
+  size = 6;
+  chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
   display = false;
 
-  generateData(num: any) {
-    this.data = buildData(num);
+  ranNum(max: number) {
+    return Math.floor(Math.random() * (max));
+  }
+
+  generateData(num: number) {
+    for (let i = 0; i < num; i++) {
+      let str = "";
+      for (let j = 0; j < this.size; j++) {
+        str += this.chars.charAt(this.ranNum(this.chars.length));
+      }
+      this.data[i] = str;
+    }
     localStorage.setItem("data", JSON.stringify(this.data));
     this.data = [];
+  }
+
+  updateData(num: number) {
+    var str_data = localStorage.getItem("data");
+    if (str_data !== null) {
+      this.data = JSON.parse(str_data);
+      if (num > this.data.length) {
+        num = this.data.length;
+      }
+      for (let i = 0; i < num; i++) {
+        let str = "";
+        for (let j = 0; j < this.size; j++) {
+          str += this.chars.charAt(this.ranNum(this.chars.length));
+        }
+        this.data[i] = str;
+      }
+      localStorage.setItem("data", JSON.stringify(this.data));
+      this.data = [];
+    }
   }
 
   clearData() {
